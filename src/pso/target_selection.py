4c61d4a5305c68281693693c01bc54cca7320aaf
tbryan2 <timbryan0315@gmail.com>
2023-12-16 11:40:20 -0600

    adding index return to function

--- src/pso/target_selection.py
+++ src/pso/target_selection.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-def select_common_target(particle_positions, candidate_targets, theta_max, w_d=1.0, w_theta=1.0):
+def select_common_target_modified(particle_positions, candidate_targets, theta_max, w_d=1.0, w_theta=1.0):
     best_score = np.inf
     best_target = None
+    best_target_index = -1  # Variable to store the index of the best target
+    horizontal_axis = np.array([1, 0])  # Horizontal axis vector
 
-    for target in candidate_targets:
+    for index, target in enumerate(candidate_targets):
         total_score = 0
 
         for particle_position in particle_positions:
@@ -14,14 +16,8 @@ def select_common_target(particle_positions, candidate_targets, theta_max, w_d=1
             # Calculate the angle using arctan2
             angle = np.arctan2(target_vector[1], target_vector[0])
 
-            angle = abs(angle) - np.pi / 2
-
-            # print(
-            #     f'Particle position: {particle_position}, Target: {target}, Distance: {distance}, Angle: {np.degrees(angle)}'
-            # )
-
             # Check if the angle is within the allowed range
-            if angle < theta_max:
+            if angle < -theta_max or angle > theta_max:
                 # If the angle is not within the allowed range, give a high penalty score
                 # total_score += 1e6  # A large number to represent an infeasible option
                 pass
@@ -32,5 +28,6 @@ def select_common_target(particle_positions, candidate_targets, theta_max, w_d=1
         if total_score < best_score:
             best_score = total_score
             best_target = target
+            best_target_index = index  # Update the index of the best target
 
-    return best_target
+    return best_target, best_target_index
