423b78c570b7ae548fc26d0e1f29e054b9453906
tbryan2 <timbryan0315@gmail.com>
2024-01-06 14:34:54 -0600

    new target selection

--- src/pso/target_selection.py
+++ src/pso/target_selection.py
@@ -1,16 +1,29 @@
 import numpy as np
 
-def select_common_target(particle_positions, candidate_targets, theta_max=30, w_d=1.0, w_theta=1.0):
+def select_common_target(particle_positions, 
+                         candidate_targets, 
+                         theta_max=30, 
+                         w_d=1.0, 
+                         w_theta=1.0,
+                         min_velocity=[3, 3]):
     best_score = np.inf
     best_target = None
     best_target_index = -1  # Variable to store the index of the best target
-    horizontal_axis = np.array([1, 0])  # Horizontal axis vector
 
     for index, target in enumerate(candidate_targets):
         total_score = 0
 
         for particle_position in particle_positions:
             distance = np.linalg.norm(target - particle_position)
+            # Calculate whether the particle can reach the targets location in the given number of frames, or skip
+            x_distance = abs(target[0] - particle_position[0])
+            y_distance = abs(target[1] - particle_position[1])
+            frame_num = index + 1
+
+            if x_distance > min_velocity[0] * frame_num or y_distance > min_velocity[1] * frame_num:
+                print('Target location infeasible: ', target)
+                total_score=np.inf + 1
+                break
             target_vector = target - particle_position
             
             # Calculate the angle using arctan2
